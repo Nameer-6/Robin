4146a13c6ead7c910e499207e418650972c84e36
Zubair Alam <syedzubairalam123@gmail.com>
2025-03-23 14:38:05 +0500

    Reporting

--- src/test/Runners/TestRunner.java
+++ src/test/Runners/TestRunner.java
@@ -6,7 +6,9 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        plugin = {"pretty", "html:target/cucumber-reports.html"}
+        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber-reports/Cucumber.json"}
 )
+monochrome = true
+
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
